4ae5399249eeae628d9aa295a4cab3d01fa9294a
Grayson Crawford <crawfordg@jbu.edu>
2023-10-11 20:21:28 -0500

    Commented it up

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,4 +1,5 @@
 package com.example;
+
 import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.speech.v1.*;
@@ -8,7 +9,12 @@ import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
 import com.google.protobuf.ByteString;
 
+import io.opencensus.common.ServerStatsFieldEnums.Size;
+
 import javax.sound.sampled.*;
+
+import org.checkerframework.common.returnsreceiver.qual.This;
+
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
@@ -16,6 +22,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         String jsonKeyPath = "wazuh-393418-3cd3bc23ce58.json";
         String languageCode = "en-US"; // Change to your desired language code
+        String translatedLanguage = "es";
+
 
         // Initialize the SpeechClient using the JSON key file
         SpeechClient speechClient = SpeechClient.create(SpeechSettings.newBuilder()
@@ -31,8 +39,17 @@ public class App {
 
 
         // Set up audio input stream from microphone
+
+        // Sample Rate: 16000 Hz (samples per second). This sets the number of audio samples to be captured per second.
+        // Sample Size In Bits: 16 bits. This specifies the number of bits used to represent each audio sample.
+        // Channels: 1. This indicates mono audio.
+        // Signed: true.
+        // Big-Endian: false. It specifies the byte order for the audio data.
+
         AudioFormat format = new AudioFormat(16000, 16, 1, true, false);
+        //TargetDataLine is a part of the Java Sound API and is used to capture audio data
         TargetDataLine microphone;
+        //Tries to find input microphone. If it cannot, it shuts down the code.
         try {
             microphone = AudioSystem.getTargetDataLine(format);
             microphone.open(format);
@@ -41,8 +58,11 @@ public class App {
             return;
         }
 
+        // This is used to store the captured audio data in memory.
         ByteArrayOutputStream audioBuffer = new ByteArrayOutputStream();
+        // It defines how much audio data will be captured at once.
         int bufferSize = 4096;
+        // Temporarily stores audio data
         byte[] buffer = new byte[bufferSize];
 
         // Start capturing audio from the microphone
@@ -50,12 +70,14 @@ public class App {
         microphone.start();
 
         while (true) {
+            //Takes the amount of bytes read from the microphone and stores it
             int bytesRead = microphone.read(buffer, 0, buffer.length);
+            // As long as the microphone is taking in audio, it will continuously write to the audioBuffer object
             if (bytesRead > 0) {
                 audioBuffer.write(buffer, 0, bytesRead);
             }
 
-            // Check for Ctrl+C to stop capturing
+            // Check for Q to stop capturing
             if (System.in.available() > 0) {
                 int key = System.in.read();
                 if (key == 'Q' || key == 'q') {
@@ -68,28 +90,39 @@ public class App {
         microphone.stop();
         microphone.close();
 
+
         // Configure recognition request
+
+        // ByteString is a data structure used in the Google Cloud API to represent binary data.
         ByteString audioBytes = ByteString.copyFrom(audioBuffer.toByteArray());
+
         RecognitionConfig config = RecognitionConfig.newBuilder()
+                //Specifies the audio encoding format, which is set to LINEAR16, indicating 16-bit linear PCM encoding.
                 .setEncoding(AudioEncoding.LINEAR16)
+                // Sets the sample rate to 16,000 Hz, matching the sample rate of the captured audio.
                 .setSampleRateHertz(16000)
+                // Sets the language code for the recognition. It's based on the language you want to recognize (e.g., "en-US" for US English).
                 .setLanguageCode(languageCode)
                 .build();
 
+        // RecognitionAudio is a data structure used in the Google Cloud API to detect audio from bytes.
         RecognitionAudio audio = RecognitionAudio.newBuilder()
                 .setContent(audioBytes)
                 .build();
 
-        // Perform the speech recognition
+        // Perform the speech recognition from Google Cloud API
         RecognizeResponse response = speechClient.recognize(config, audio);
 
         // Print transcription results
         for (SpeechRecognitionResult result : response.getResultsList()) {
+            // For each recognition result, you extract the recognized transcript (text) from the 
+            // first alternative (the most likely transcription) and store it
             String speech = result.getAlternatives(0).getTranscript();
+            // Translate the speech
             Translation translation = translate.translate(
                 speech, Translate.TranslateOption.sourceLanguage("en"),
-                Translate.TranslateOption.targetLanguage("es"));
-            System.out.println("Transcript: " + translation.getTranslatedText());
+                Translate.TranslateOption.targetLanguage(translatedLanguage));
+            System.out.println("Transcript in '" + translatedLanguage + "'':" + translation.getTranslatedText());
         }
 
         // Close the SpeechClient
