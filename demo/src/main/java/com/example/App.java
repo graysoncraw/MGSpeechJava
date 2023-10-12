bd53d862b4c07d8b00b9353dfa377659a46d9aed
Grayson Crawford <crawfordg@jbu.edu>
2023-10-11 20:05:43 -0500

    Translate+Speech work

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.speech.v1.*;
 import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
 import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
 import com.google.protobuf.ByteString;
 
@@ -12,7 +13,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
 public class App {
-    private static Translate translate;
     public static void main(String[] args) throws Exception {
         String jsonKeyPath = "wazuh-393418-3cd3bc23ce58.json";
         String languageCode = "en-US"; // Change to your desired language code
@@ -21,6 +21,14 @@ public class App {
         SpeechClient speechClient = SpeechClient.create(SpeechSettings.newBuilder()
                 .setCredentialsProvider(FixedCredentialsProvider.create(ServiceAccountCredentials.fromStream(getCredentialStream(jsonKeyPath))))
                 .build());
+
+        // Initialize the Translate using the JSON key file
+        ServiceAccountCredentials credentials = ServiceAccountCredentials.fromStream(getCredentialStream(jsonKeyPath));
+        Translate translate = TranslateOptions.newBuilder()
+                .setCredentials(credentials)
+                .build()
+                .getService();
+
 
         // Set up audio input stream from microphone
         AudioFormat format = new AudioFormat(16000, 16, 1, true, false);
@@ -80,8 +88,7 @@ public class App {
             String speech = result.getAlternatives(0).getTranscript();
             Translation translation = translate.translate(
                 speech, Translate.TranslateOption.sourceLanguage("en"),
-                Translate.TranslateOption.targetLanguage("es"),
-                Translate.TranslateOption.model("base"));
+                Translate.TranslateOption.targetLanguage("es"));
             System.out.println("Transcript: " + translation.getTranslatedText());
         }
 
